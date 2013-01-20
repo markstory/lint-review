bf6a1fa52e70c1feb9f1c27eb21b17b99fcc4aff
Mark Story <mark@mark-story.com>
2013-01-20 15:55:11 -0500

    Add basic things.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "Lint Review"
+VERSION = "0.0.1"
+
+requirements = open('./requirements.txt', 'r')
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description="""
+    Lint Review, an automated code review tool that integrates with github.
+    Integrates with the github API & a variety of code checking tools.
+    """,
+    author="Mark story",
+    author_email="mark@mark-story.com",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'webserver = lintreview.web:main',
+        ],
+    },
+    install_requires=requirements.readlines(),
+)
