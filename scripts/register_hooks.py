3d0eca09fb18c77c6837c0cbae8064d133cb5664
Mark Story <mark@mark-story.com>
2013-01-20 21:04:18 -0500

    use argparse instead of optparse
    optparse is deprecated, use the newer library instead.

--- scripts/register_hooks.py
+++ scripts/register_hooks.py
@@ -3,10 +3,14 @@
 # Register webhooks for a github repository
 #
 # Uses the settings and configuration defined
+import argparse
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user', help="The user or organization the repo is under.")
+    parser.add_argument('repo', help="The repository to install a hook into.")
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
--- scripts/runserver.py
+++ scripts/runserver.py
@@ -3,26 +3,36 @@
 # Run the flask application that accepts
 # webhooks from github.
 from lintreview.web import app
-from optparse import OptionParser
+import argparse
 
 
 def main():
-    parser = OptionParser()
-    parser.add_option('-d', '--debug',
-                      action='store_true', dest='debug', default=False,
-                      help='Enable the flask debugger.')
-    parser.add_option('-b', '--bind-address',
-                      action='store', dest='host', default='0.0.0.0',
-                      type='string',
-                      help='The ip address to bind to.')
-    parser.add_option('-p', '--port',
-                      action='store', dest='port', default='5000',
-                      type='int',
-                      help='The port to bind onto.')
-    (options, args) = parser.parse_args()
+    desc = """Start the lintreview webserver.
+    It handles webhooks from github.
+    """
 
-    app.debug = options.debug
-    app.run(host=options.host, port=options.port)
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-d, --debug',
+            action="store_true",
+            dest='debug',
+            default=False,
+            help='Enable the flask debugger.')
+    parser.add_argument('-b, --bind-address',
+            action='store',
+            dest='host',
+            default='0.0.0.0',
+            type=str,
+            help='The ip address to bind to.')
+    parser.add_argument('-p, --port',
+            action='store',
+            dest='port',
+            default='5000',
+            type=int,
+            help='The port to bind onto.')
+    args = parser.parse_args()
+
+    app.debug = args.debug
+    app.run(host=args.host, port=args.port)
 
 
 if __name__ == '__main__':
