79f8af43d5783227b59ab5cac9be0b02e5adc4bb
Mark Story <mark@mark-story.com>
2013-02-02 13:50:51 -0500

    Start adding admin user options.
    It is unlikely that the user running lint review will also be the admin
    user of the repositories. Add more cli flags to allow easier setup.

--- scripts/register_hooks.py
+++ scripts/register_hooks.py
@@ -2,7 +2,6 @@
 
 # Register webhooks for a github repository
 #
-# Uses the settings and configuration defined
 from lintreview.web import app
 from lintreview.github import register_hook
 import argparse
@@ -15,6 +14,15 @@ def main():
     reviews as pull requests are opened/updated.
     """
     parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-u', '--user',
+                        dest='login_user',
+                        help="The user that has admin rights to the repo you "
+                             "are adding hooks to. Useful when the user in "
+                             "settings is not the administrator of "
+                             "your repositories.")
+    parser.add_argument('-p', '--password',
+                        dest='login_pass',
+                        help="The password of the admin user.")
     parser.add_argument('user',
                         help="The user or organization the repo is under.")
     parser.add_argument('repo',
