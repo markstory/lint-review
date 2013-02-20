f411649ce4d3bb4382d4d097edc712a1c1839455
Mark Story <mark@mark-story.com>
2013-02-19 22:40:53 -0500

    Start new single entry point cli tool.
    This will eventually replace scripts/register_hook.

---
+++ scripts/lintreview.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import argparse
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+
+def create_parser():
+    desc = """
+    Command line utilities for lintreview.
+    """
+    parser = argparse.ArgumentParser(description=desc)
+
+    commands = parser.add_subparsers(
+        title="Subcommands",
+        description="Valid subcommands")
+
+    desc = """
+    Register webhooks for a given user & repo
+    The installed webhook will be used to trigger lint
+    reviews as pull requests are opened/updated.
+    """
+    register = commands.add_parser('register', help=desc)
+    register.add_argument(
+        '-u',
+        '--user',
+        dest='login_user',
+        help="The user that has admin rights to the repo "
+             "you are adding hooks to. Useful when the user "
+             "in settings is not the administrator of "
+             "your repositories.")
+    register.add_argument(
+        '-p',
+        '--password',
+        dest='login_pass',
+        help="The password of the admin user.")
+    register.add_argument('user',
+                          help="The user or organization the repo is under.")
+    register.add_argument('repo',
+                          help="The repository to install a hook into.")
+
+    desc = """
+    Unregister webhooks for a given user & repo.
+    """
+    remove = commands.add_parser('unregister', help=desc)
+    remove.add_argument(
+        '-u', '--user',
+        dest='login_user',
+        help="The user that has admin rights to the repo you "
+             "are removing hooks from. Useful when the "
+             "user in settings is not the administrator of "
+             "your repositories.")
+    remove.add_argument(
+        '-p',
+        '--password',
+        dest='login_pass',
+        help="The password of the admin user.")
+    remove.add_argument('user',
+                        help="The user or organization the repo is under.")
+    remove.add_argument('repo',
+                        help="The repository to remove a hook from.")
+
+    return parser
+
+if __name__ == '__main__':
+    main()
