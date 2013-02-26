7599c0b9a8dd302440d8ea0289bd72391349abf6
Mark Story <mark@mark-story.com>
2013-02-25 22:53:13 -0500

    Add tests for registering hooks.

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,7 +1,12 @@
 import lintreview.github as github
 
-from pygithub3 import Github
+from . import load_fixture
+from mock import call
+from mock import patch
+from mock import Mock
 from nose.tools import eq_
+from pygithub3 import Github
+from requests.models import Response
 
 
 config = {
@@ -23,12 +28,45 @@ def test_get_lintrc():
     assert isinstance(lintrc, str)
 
 
-def test_register_hook():
-    pass
+@patch('pygithub3.core.client.Client.get')
+def test_register_hook(http):
+    response = Response()
+    response._content = '[]'
+    http.return_value = response
+
+    gh = Github()
+    gh.repos.hooks.create = Mock()
+    url = 'http://example.com/review/start'
+
+    github.register_hook(gh, url, 'mark', 'lint-test')
+
+    assert gh.repos.hooks.create.called, 'Create not called'
+    calls = gh.repos.hooks.create.call_args_list
+    expected = call({
+        'name': 'web',
+        'active': True,
+        'config': {
+            'content_type': 'json',
+            'url': url,
+        },
+        'events': ['pull_request']
+    }, user='mark', repo='lint-test')
+    eq_(calls[0], expected)
 
 
-def test_register_hook__already_exists():
-    pass
+@patch('pygithub3.core.client.Client.get')
+def test_register_hook__already_exists(http):
+    response = Response()
+    response._content = load_fixture('webhook_list.json')
+    http.return_value = response
+
+    gh = Github()
+    gh.repos.hooks.create = Mock()
+    url = 'http://example.com/review/start'
+
+    github.register_hook(gh, url, 'mark', 'lint-test')
+
+    assert gh.repos.hooks.create.called is False, 'Create called'
 
 
 def test_register_hook__failed():
