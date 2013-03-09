2b35d1e2892ea23d18243b6dcbdb4d4c10ad4cc8
Mark Story <mark@freshbooks.com>
2013-03-09 14:16:25 -0500

    Update tests to pass with jshint 1.1.0

--- tests/tools/test_jshint.py
+++ tests/tools/test_jshint.py
@@ -39,7 +39,7 @@ class TestJshint(TestCase):
     def test_process_files__one_file_fail(self):
         self.tool.process_files([self.fixtures[1]])
         problems = self.problems.all(self.fixtures[1])
-        eq_(13, len(problems))
+        eq_(8, len(problems))
 
         fname = self.fixtures[1]
         expected = (fname, 1, 'Missing name in function declaration.')
@@ -49,7 +49,7 @@ class TestJshint(TestCase):
         eq_(expected, problems[2])
 
         expected = (fname, 7, "Implied global 'alert'")
-        eq_(expected, problems[11])
+        eq_(expected, problems[6])
 
     @skipIf(jshint_missing, 'Missing jshint, cannot run')
     def test_process_files_two_files(self):
@@ -58,7 +58,7 @@ class TestJshint(TestCase):
         eq_([], self.problems.all(self.fixtures[0]))
 
         problems = self.problems.all(self.fixtures[1])
-        eq_(13, len(problems))
+        eq_(8, len(problems))
 
     @skipIf(jshint_missing, 'Missing jshint, cannot run')
     def test_process_files_with_config(self):
@@ -70,4 +70,4 @@ class TestJshint(TestCase):
 
         problems = self.problems.all(self.fixtures[1])
 
-        eq_(10, len(problems), 'Config file should lower error count.')
+        eq_(6, len(problems), 'Config file should lower error count.')
