69b08c1ea6f708e5ab24d80fc5697d82176cf44a
duncan <duncanw@freshbooks.com>
2014-05-08 12:18:05 -0400

    Adds puppet-lint support
    Based upon
    https://github.com/markstory/lint-review/commit/fc1afddc3d63263b520ae37fdba6bd457cf48e5b

---
+++ lintreview/tools/puppet.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from lintreview.tools import Tool
+from lintreview.tools import run_command
+from lintreview.utils import in_path
+
+log = logging.getLogger(__name__)
+
+
+class Puppet(Tool):
+
+    name = 'puppet-lint'
+
+    def check_dependencies(self):
+        """
+        See if puppet-lint is on the PATH
+        """
+        return in_path('puppet-lint')
+
+    def match_file(self, filename):
+        base = os.path.basename(filename)
+        name, ext = os.path.splitext(base)
+        return ext == '.pp'
+
+    def process_files(self, files):
+        """
+        Run code checks with puppet-lint
+        """
+        log.debug('Processing %s files with %s', files, self.name)
+        command = [self.name, '--log-format',
+                   '%{path}:%{linenumber}:%{KIND}:%{message}']
+        command += files
+        output = run_command(
+            command,
+            split=True,
+            ignore_error=True,
+            include_errors=False
+        )
+
+        if not output:
+            log.debug('No puppet-lint errors found.')
+            return False
+
+        for line in output:
+            filename, line, error = self._parse_line(line)
+            self.problems.add(filename, line, error)
+
+    def _parse_line(self, line):
+        """
+        `rubocop --format emacs` lines look like this:
+        filename:lineno:charno: error-type: error
+        """
+        parts = line.split(':', 2)
+        message = parts[2].strip()
+        filename = os.path.abspath(parts[0])
+        return (filename, int(parts[1]), message)
---
+++ tests/tools/test_puppet.py
@@ -0,0 +1,57 @@
+from os.path import abspath
+from lintreview.review import Problems
+from lintreview.review import Comment
+from lintreview.utils import in_path
+from lintreview.tools.puppet import Puppet
+from unittest import TestCase
+from unittest import skipIf
+from nose.tools import eq_
+from operator import attrgetter
+
+puppet_missing = not(in_path('puppet-lint'))
+
+
+class TestPuppet(TestCase):
+    needs_puppet = skipIf(puppet_missing, 'Missing puppet-lint, cannot run')
+
+    fixtures = [
+        'tests/fixtures/puppet/no_errors.pp',
+        'tests/fixtures/puppet/has_errors.pp',
+    ]
+
+    def setUp(self):
+        self.problems = Problems()
+        self.tool = Puppet(self.problems)
+
+    def test_match_file(self):
+        self.assertFalse(self.tool.match_file('test.py'))
+        self.assertFalse(self.tool.match_file('dir/name/test.py'))
+        self.assertTrue(self.tool.match_file('test.pp'))
+        self.assertTrue(self.tool.match_file('dir/name/test.pp'))
+
+    @needs_puppet
+    def test_process_files__one_file_pass(self):
+        self.tool.process_files([self.fixtures[0]])
+        eq_([], self.problems.all(self.fixtures[0]))
+
+    @needs_puppet
+    def test_process_files__one_file_fail(self):
+        filename = abspath(self.fixtures[1])
+        self.tool.process_files([filename])
+        expected_problems = [
+            Comment(filename, 2, 2, 'ERROR:foo not in autoload module layout'),
+            Comment(filename, 3, 3, 'ERROR:trailing whitespace found'),
+        ]
+
+        problems = sorted(self.problems.all(filename), key=attrgetter('line'))
+        eq_(expected_problems, problems)
+
+    @needs_puppet
+    def test_process_files_two_files(self):
+        self.tool.process_files(self.fixtures)
+
+        linty_filename = abspath(self.fixtures[1])
+        eq_(2, len(self.problems.all(linty_filename)))
+
+        freshly_laundered_filename = abspath(self.fixtures[0])
+        eq_([], self.problems.all(freshly_laundered_filename))
