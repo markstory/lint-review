8d64cc4bb48b4983c7d026a2e9cce5d5f6b79fb7
William Stewart <william@synthasite.com>
2014-08-13 10:37:44 +0200

    Fix tests with new extra argument for clone_or_update

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,6 +1,7 @@
 import lintreview.git as git
 import os
 import shutil
+from test_github import config
 from nose.tools import eq_
 from nose.tools import raises
 from unittest import skipIf
@@ -80,6 +81,7 @@ def test_clone_or_update():
 
     assert not(git.exists(path)), 'Directory should not exist.'
     git.clone_or_update(
+        config,
         'git://github.com/markstory/lint-review.git',
         path,
         'e4f880c77e6b2c81c81cad5d45dd4e1c39b919a0')
