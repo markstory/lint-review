078cca7dbcc052b9469d5a8bf70f357ca939e61b
Mark Story <mark@mark-story.com>
2016-02-21 08:55:18 -0500

    Update preocessor and its tests.

--- lintreview/processor.py
+++ lintreview/processor.py
@@ -11,27 +11,25 @@ log = logging.getLogger(__name__)
 class Processor(object):
 
     _repository = None
-    _number = None
-    _head = None
+    _pull_request = None
     _target_path = None
     _changes = None
     _problems = None
     _review = None
     _config = None
 
-    def __init__(self, repository, number, head, target_path, config=None):
+    def __init__(self, repository, pull_request, target_path, config=None):
         config = config if config else {}
         self._config = config
         self._repository = repository
-        self._number = number
-        self._head = head
+        self._pull_request = pull_request
         self._target_path = target_path
         self._problems = Problems(target_path)
-        self._review = Review(repository, number, config)
+        self._review = Review(repository, pull_request, config)
 
     def load_changes(self):
         log.info('Loading pull request patches from github.')
-        files = list(self._repository.pull_request(self._number).files())
+        files = self._pull_request.files()
         self._changes = DiffCollection(files)
         self._problems.set_changes(self._changes)
 
@@ -42,7 +40,7 @@ class Processor(object):
         files_to_check = self._changes.get_files(
             append_base=self._target_path,
             ignore_patterns=review_config.ignore_patterns())
-        commits_to_check = self.get_commits(self._number)
+        commits_to_check = self._pull_request.commits()
         tools.run(
             review_config,
             self._problems,
@@ -54,8 +52,5 @@ class Processor(object):
         self._problems.limit_to_changes()
         self._review.publish(
             self._problems,
-            self._head,
+            self._pull_request.head,
             self._config.get('SUMMARY_THRESHOLD'))
-
-    def get_commits(self, number):
-        return self._repository.pull_request(number).commits()
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -13,20 +13,20 @@ fixture_data = load_fixture('one_file_pull_request.json')
 
 class ProcessorTest(TestCase):
 
-    def get_mock_client(self, fixture):
-        gh = Mock()
-        pull_request = Mock()
+    def get_pull_request(self, fixture):
+        pull_request = Mock(number=1, head='123abc')
 
         pull_request.files.return_value = map(
             lambda f: PullFile(f),
             json.loads(fixture))
 
-        gh.pull_request.return_value = pull_request
-        return gh
+        return pull_request
 
     def test_load_changes(self):
-        gh = self.get_mock_client(fixture_data)
-        subject = Processor(gh, 1, '123abc', './tests')
+        pull = self.get_pull_request(fixture_data)
+        repo = Mock()
+
+        subject = Processor(repo, pull, './tests')
         subject.load_changes()
 
         eq_(1, len(subject._changes), 'File count is wrong')
@@ -34,17 +34,19 @@ class ProcessorTest(TestCase):
 
     @raises(RuntimeError)
     def test_run_tools__no_changes(self):
-        gh = self.get_mock_client(fixture_data)
+        pull = self.get_pull_request(fixture_data)
+        repo = Mock()
 
-        subject = Processor(gh, 1, '123abc', './tests')
+        subject = Processor(repo, pull, './tests')
         subject.run_tools(None)
 
     @patch('lintreview.processor.tools')
     def test_run_tools(self, tool_stub):
-        gh = self.get_mock_client(load_fixture('commits.json'))
+        pull = self.get_pull_request(load_fixture('commits.json'))
+        repo = Mock()
 
         stub_config = Mock()
-        subject = Processor(gh, 1, '123abc', './tests')
+        subject = Processor(repo, pull, './tests')
         subject._changes = Mock()
         subject.run_tools(stub_config)
         assert tool_stub.run.called, 'Should have ran'
@@ -52,9 +54,10 @@ class ProcessorTest(TestCase):
         assert stub_config.ignore_patterns.called
 
     def test_publish(self):
-        gh = self.get_mock_client(load_fixture('commits.json'))
+        pull = self.get_pull_request(load_fixture('commits.json'))
+        repo = Mock()
 
-        subject = Processor(gh, 1, '123abc', './tests', {'SUMMARY_THRESHOLD': 50})
+        subject = Processor(repo, pull, './tests', {'SUMMARY_THRESHOLD': 50})
         subject._problems = Mock()
         subject._review = Mock()
 
