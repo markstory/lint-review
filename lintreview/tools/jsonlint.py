a8b39f7d8ba62917078e1356126ceadf7b02cbb5
Duncan Ward <git@dub.to>
2016-04-20 16:31:14 -0400

    Enable JSON linting
    using the python module demjson which provides a jsonlint script
    Which prevents needing to install an external npm module and also the
    npm module seems to stop after the first warning.

---
+++ lintreview/tools/jsonlint.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from lintreview.tools import Tool
+from lintreview.tools import run_command
+from lintreview.utils import in_path
+
+log = logging.getLogger(__name__)
+
+
+class JSONlint(Tool):
+
+    name = 'jsonlint'
+
+    def check_dependencies(self):
+        """
+        See if jsonlint is on the PATH
+        """
+        return in_path('jsonlint')
+
+    def match_file(self, filename):
+        base = os.path.basename(filename)
+        name, ext = os.path.splitext(base)
+        return ext == '.json'
+
+    def process_files(self, files):
+        """
+        Run code checks with jsonlint.
+        Only a single process is made for all files
+        to save resources.
+        Configuration is not supported at this time
+        """
+        log.debug('Processing %s files with %s', files, self.name)
+
+        command = ['jsonlint']
+        command += files
+
+        output = run_command(command, split=True, ignore_error=True)
+        if not output:
+            log.debug('No jsonlint errors found.')
+            return False
+
+        for line in output:
+            if (line[0] == ' ' or
+                    line.find(': has errors') >= 0 or
+                    line.find(': ok') >= 0):
+                continue
+            filename, line, error = self._parse_line(line)
+            self.problems.add(filename, line, error)
+
+    def _parse_line(self, line):
+        """
+        jsonlint only generates results as stdout.
+        Parse the output for real data.
+        """
+        parts = line.split(':', 3)
+        return (parts[0], int(parts[1]), parts[3][1:-1])
---
+++ tests/tools/test_jsonlint.py
@@ -0,0 +1,77 @@
+from lintreview.review import Problems
+from lintreview.review import Comment
+from lintreview.tools.jsonlint import JSONlint
+from unittest import TestCase
+from nose.tools import eq_
+
+
+class TestJSONlint(TestCase):
+
+    fixtures = [
+        'tests/fixtures/jsonlint/no_errors.json',
+        'tests/fixtures/jsonlint/has_warnings.json',
+        'tests/fixtures/jsonlint/has_errors.json',
+    ]
+
+    def setUp(self):
+        self.problems = Problems()
+        self.tool = JSONlint(self.problems)
+
+    def test_match_file(self):
+        self.assertFalse(self.tool.match_file('test.php'))
+        self.assertFalse(self.tool.match_file('test.js'))
+        self.assertFalse(self.tool.match_file('dir/name/test.js'))
+        self.assertTrue(self.tool.match_file('test.json'))
+        self.assertTrue(self.tool.match_file('dir/name/test.json'))
+
+    def test_process_files__one_file_pass(self):
+        self.tool.process_files([self.fixtures[0]])
+        eq_([], self.problems.all(self.fixtures[0]))
+
+    def test_process_files__one_file_fail(self):
+        self.tool.process_files([self.fixtures[1]])
+        problems = self.problems.all(self.fixtures[1])
+        eq_(2, len(problems))
+
+        fname = self.fixtures[1]
+
+        msg = ("Warning: String literals must use "
+               "double quotation marks in strict JSON")
+        expected = Comment(fname, 2, 2, msg)
+        eq_(expected, problems[0])
+
+        msg = ("Warning: JSON does not allow identifiers "
+               "to be used as strings: u'three'\n"
+               "Warning: Strict JSON does not allow a final comma "
+               "in an object (dictionary) literal")
+        expected = Comment(fname, 3, 3, msg)
+        eq_(expected, problems[1])
+
+    def test_process_files_three_files(self):
+        self.tool.process_files(self.fixtures)
+
+        eq_([], self.problems.all(self.fixtures[0]))
+
+        fname = self.fixtures[1]
+        problems = self.problems.all(fname)
+        eq_(2, len(problems))
+
+        msg = ("Warning: String literals must use "
+               "double quotation marks in strict JSON")
+        expected = Comment(fname, 2, 2, msg)
+        eq_(expected, problems[0])
+
+        msg = ("Warning: JSON does not allow identifiers "
+               "to be used as strings: u'three'\n"
+               "Warning: Strict JSON does not allow a final comma "
+               "in an object (dictionary) literal")
+        expected = Comment(fname, 3, 3, msg)
+        eq_(expected, problems[1])
+
+        fname = self.fixtures[2]
+        problems = self.problems.all(fname)
+        eq_(1, len(problems))
+
+        msg = "Error: Unknown identifier: u'bleugh'"
+        expected = Comment(fname, 1, 1, msg)
+        eq_(expected, problems[0])
