4d17278bb340ef5fadee046eb12dce42632d8c27
Mark Story <mark@mark-story.com>
2017-01-23 22:27:49 -0500

    Fix pypi errors

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ requirements = open('./requirements.txt', 'r')
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
-    description="""
-    Lint Review, an automated code review tool that integrates with github.
-    Integrates with the github API & a variety of code checking tools.
-    """,
+    description="Lint Review, an automated code review tool that "
+                "integrates with github. Integrates with the github API "
+                "& a variety of code checking tools.",
     author="Mark story",
     author_email="mark@mark-story.com",
     packages=find_packages(),
