f23c417509323e53df28fb41467e82877fd329fc
Mark Story <mark@mark-story.com>
2017-05-11 22:20:54 -0400

    Add checkstyle linter.
    Enables checking java files with the checkstyle code style tool.

---
+++ lintreview/tools/checkstyle.py
@@ -0,0 +1,62 @@
+import functools
+import logging
+import os
+from lintreview.review import IssueComment
+from lintreview.tools import Tool
+from lintreview.tools import run_command, process_checkstyle
+from lintreview.utils import in_path
+
+log = logging.getLogger(__name__)
+
+
+class Checkstyle(Tool):
+
+    name = 'checkstyle'
+
+    def check_dependencies(self):
+        """
+        See if checkstyle is on the system path.
+        """
+        return in_path('checkstyle')
+
+    def match_file(self, filename):
+        base = os.path.basename(filename)
+        name, ext = os.path.splitext(base)
+        return ext == '.java'
+
+    def process_files(self, files):
+        """
+        Run code checks with checkstyle.
+        Only a single process is made for all files
+        to save resources.
+        """
+        log.debug('Processing %s files with %s', files, self.name)
+        if 'config' not in self.options:
+            msg = ("We could not run `checkstyle` you did not set "
+                   "the `config` option to a valid checkstyle XML file.")
+            return self.problems.add(IssueComment(msg))
+        command = self.create_command(files)
+        output = run_command(
+            command,
+            ignore_error=True)
+
+        # Remove the last line if it is not XML
+        # Checkstyle outputs text after the XML if there are errors.
+        lines = output.strip().split('\n')
+        if not lines[-1].strip().startswith('<'):
+            lines = lines[0:-1]
+        output = ''.join(lines)
+
+        filename_converter = functools.partial(
+            self._relativize_filename,
+            files)
+        process_checkstyle(self.problems, output, filename_converter)
+
+    def create_command(self, files):
+        command = [
+            'checkstyle',
+            '-f', 'xml',
+            '-c', self.apply_base(self.options['config'])
+        ]
+        command += files
+        return command
---
+++ tests/tools/test_checkstyle.py
@@ -0,0 +1,101 @@
+from lintreview.review import Problems
+from lintreview.review import Comment
+from lintreview.tools.checkstyle import Checkstyle
+from lintreview.utils import in_path
+from unittest import TestCase
+from unittest import skipIf
+from nose.tools import eq_, assert_in
+
+checkstyle_missing = not(in_path('checkstyle'))
+
+
+class TestCheckstyle(TestCase):
+
+    needs_checkstyle = skipIf(checkstyle_missing, 'Needs checkstyle to run')
+
+    fixtures = [
+        'tests/fixtures/checkstyle/no_errors.java',
+        'tests/fixtures/checkstyle/has_errors.java',
+    ]
+
+    def setUp(self):
+        self.problems = Problems()
+        config = {
+            'config': 'tests/fixtures/checkstyle/config.xml'
+        }
+        self.tool = Checkstyle(self.problems, config)
+
+    def test_match_file(self):
+        self.assertFalse(self.tool.match_file('test.php'))
+        self.assertFalse(self.tool.match_file('dir/name/test.py'))
+        self.assertFalse(self.tool.match_file('test.py'))
+        self.assertTrue(self.tool.match_file('test.java'))
+        self.assertTrue(self.tool.match_file('dir/name/test.java'))
+
+    @needs_checkstyle
+    def test_check_dependencies(self):
+        self.assertTrue(self.tool.check_dependencies())
+
+    @needs_checkstyle
+    def test_process_files__one_file_pass(self):
+        self.tool.process_files([self.fixtures[0]])
+        eq_([], self.problems.all(self.fixtures[0]))
+
+    @needs_checkstyle
+    def test_process_files__multiple_error(self):
+        self.tool.process_files([self.fixtures[1]])
+        problems = self.problems.all(self.fixtures[1])
+        eq_(4, len(problems))
+
+        fname = self.fixtures[1]
+
+        expected = Comment(
+            fname,
+            1,
+            1,
+            "Missing a Javadoc comment.\n"
+            "Utility classes should not have a public or default constructor.")
+        eq_(expected, problems[0])
+
+        expected = Comment(
+            fname,
+            3,
+            3,
+            "Missing a Javadoc comment.\n"
+            "Parameter args should be final."
+        )
+        eq_(expected, problems[2])
+
+    @needs_checkstyle
+    def test_process_files_two_files(self):
+        self.tool.process_files(self.fixtures)
+
+        eq_([], self.problems.all(self.fixtures[0]))
+
+        problems = self.problems.all(self.fixtures[1])
+        eq_(4, len(problems))
+
+    @needs_checkstyle
+    def test_process_files__no_config_comment(self):
+        config = {}
+        tool = Checkstyle(self.problems, config)
+        tool.process_files(self.fixtures)
+
+        problems = self.problems.all()
+        eq_(1, len(problems))
+        assert_in('could not run `checkstyle`', problems[0].body)
+
+    def test_create_command__with_path_based_standard(self):
+        config = {
+            'config': 'test/checkstyle.xml'
+        }
+        tool = Checkstyle(self.problems, config, '/some/path')
+        result = tool.create_command(['some/file.js'])
+        expected = [
+            'checkstyle',
+            '-f', 'xml',
+            '-c', '/some/path/test/checkstyle.xml',
+            'some/file.js'
+        ]
+        assert 'checkstyle' in result[0], 'checkstyle is in command name'
+        eq_(result, expected)
