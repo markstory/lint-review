d87ad8eda0bda255b8ebe1e0c17892bb795fd95f
Mark Story <mark@mark-story.com>
2017-11-07 21:41:35 -0500

    Add tests for bad path setups.

--- tests/test_git.py
+++ tests/test_git.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import lintreview.git as git
 import os
 from .test_github import config
-from nose.tools import eq_, raises, assert_in
+from nose.tools import eq_, raises, assert_in, with_setup
 from unittest import skipIf
 
 settings = {
@@ -12,9 +12,21 @@ clone_path = settings['WORKSPACE'] + '/test_clone'
 cant_write_to_test = not(os.access(os.path.abspath('./tests'), os.W_OK))
 
 
-def teardown():
+def setup_repo():
+    git.clone_or_update(
+        config,
+        'git://github.com/markstory/lint-review.git',
+        clone_path,
+        'master')
+
+
+def teardown_repo():
     if git.exists(clone_path):
         git.destroy(clone_path)
+
+
+def noop():
+    pass
 
 
 def test_get_repo_path():
@@ -60,6 +72,7 @@ def test_exists__no_git():
 
 
 @raises(IOError)
+@with_setup(noop, teardown_repo)
 def test_repo_clone_no_repo():
     git.clone(
         'git://github.com/markstory/it will never work.git',
@@ -78,6 +91,7 @@ def test_repo_operations():
 
 
 @skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@with_setup(noop, teardown_repo)
 def test_clone_or_update():
     git.clone_or_update(
         config,
@@ -88,12 +102,8 @@ def test_clone_or_update():
 
 
 @skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@with_setup(setup_repo, teardown_repo)
 def test_diff():
-    git.clone_or_update(
-        config,
-        'git://github.com/markstory/lint-review.git',
-        clone_path,
-        'master')
     with open(clone_path + '/README.mdown', 'w') as f:
         f.write('New readme')
     result = git.diff(clone_path)
@@ -104,12 +114,15 @@ def test_diff():
     assert_in('-# Lint Review', result)
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@raises(IOError)
+def test_diff__non_git_path():
+    git.diff(settings['WORKSPACE'] + '/../../')
+
+
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@with_setup(setup_repo, teardown_repo)
 def test_apply_cached():
-    git.clone_or_update(
-        config,
-        'git://github.com/markstory/lint-review.git',
-        clone_path,
-        'master')
     with open(clone_path + '/README.mdown', 'w') as f:
         f.write('New readme')
     # Get the initial diff.
@@ -121,12 +134,9 @@ def test_apply_cached():
     eq_(diff, '')
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@with_setup(setup_repo, teardown_repo)
 def test_apply_cached__empty():
-    git.clone_or_update(
-        config,
-        'git://github.com/markstory/lint-review.git',
-        clone_path,
-        'master')
     git.apply_cached(clone_path, '')
 
     # No changes, no diff.
@@ -134,22 +144,22 @@ def test_apply_cached__empty():
     eq_(diff, '')
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
 @raises(IOError)
+@with_setup(setup_repo, teardown_repo)
 def test_apply_cached__bad_patch():
-    git.clone_or_update(
-        config,
-        'git://github.com/markstory/lint-review.git',
-        clone_path,
-        'master')
     git.apply_cached(clone_path, 'not a diff')
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@raises(IOError)
+def test_apply_cached__non_git_path():
+    git.apply_cached(settings['WORKSPACE'] + '/../../', 'not a patch')
+
+
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
+@with_setup(setup_repo, teardown_repo)
 def test_commit_and_status():
-    git.clone_or_update(
-        config,
-        'git://github.com/markstory/lint-review.git',
-        clone_path,
-        'master')
     with open(clone_path + '/README.mdown', 'w') as f:
         f.write('New readme')
     diff = git.diff(clone_path)
@@ -163,13 +173,16 @@ def test_commit_and_status():
     eq_('', status, 'No changes unstaged, or uncommitted')
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
 def test_push():
     assert False, 'not done'
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
 def test_push__fails():
     assert False, 'not done'
 
 
+@skipIf(cant_write_to_test, 'Cannot write to ./tests skipping')
 def test_add_remote():
     assert False, 'not done'
