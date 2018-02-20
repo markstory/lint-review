55afc9cf1b0dbbf14fb097ae8d346d0bf5fdb9fd
Mark Story <mark@mark-story.com>
2018-02-19 23:56:23 -0500

    Move rubocop to docker.

--- lintreview/tools/rubocop.py
+++ lintreview/tools/rubocop.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import
 import os
 import logging
-from lintreview.tools import Tool
-from lintreview.tools import run_command
-from lintreview.utils import in_path
-from lintreview.utils import bundle_exists
+import lintreview.docker as docker
+from lintreview.tools import Tool, process_quickfix
 
 log = logging.getLogger(__name__)
 
@@ -15,9 +13,9 @@ class Rubocop(Tool):
 
     def check_dependencies(self):
         """
-        See if rubocop is on the PATH
+        See if ruby image exists
         """
-        return in_path('rubocop') or bundle_exists('rubocop')
+        return docker.image_exists('ruby2')
 
     def match_file(self, filename):
         base = os.path.basename(filename)
@@ -31,38 +29,22 @@ class Rubocop(Tool):
         log.debug('Processing %s files with %s', files, self.name)
         command = self._create_command()
         command += files
-        output = run_command(
-            command,
-            split=True,
-            ignore_error=True,
-            include_errors=False
-        )
+        output = docker.run('ruby2', command, self.base_path)
 
         if not output:
             log.debug('No rubocop errors found.')
             return False
 
-        for line in output:
-            filename, line, error = self._parse_line(line)
-            self.problems.add(filename, line, error)
+        output = output.split("\n")
+        process_quickfix(self.problems, output, docker.strip_base)
 
     def _create_command(self):
-        command = ['rubocop']
-        if bundle_exists('rubocop'):
-            command = ['bundle', 'exec', 'rubocop']
-        command += ['--format', 'emacs']
+        command = ['bundle', 'exec', 'rubocop', '--format', 'emacs']
         if self.options.get('display_cop_names', '').lower() == 'true':
             command.append('--display-cop-names')
+        else:
+            command.append('--no-display-cop-names')
         return command
-
-    def _parse_line(self, line):
-        """
-        `rubocop --format emacs` lines look like this:
-        filename:lineno:charno: error-type: error
-        """
-        parts = line.split(':', 3)
-        message = parts[3].strip()
-        return (parts[0], int(parts[1]), message)
 
     def has_fixer(self):
         """
@@ -74,10 +56,7 @@ class Rubocop(Tool):
         """Run Rubocop in the fixer mode.
         """
         command = self.create_fixer_command(files)
-        run_command(
-            command,
-            ignore_error=True,
-            include_errors=False)
+        docker.run('ruby2', command, self.base_path)
 
     def create_fixer_command(self, files):
         command = self._create_command()
