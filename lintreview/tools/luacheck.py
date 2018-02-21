97dbe21b2d926e74b137b84658e8cd7245a8b0e7
Mark Story <mark@mark-story.com>
2018-02-20 22:45:14 -0500

    Move luacheck to docker.

--- lintreview/tools/luacheck.py
+++ lintreview/tools/luacheck.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 import logging
 import os
 import functools
-from lintreview.tools import Tool, run_command, process_quickfix
-from lintreview.utils import in_path
+import lintreview.docker as docker
+from lintreview.tools import Tool, process_quickfix
 
 log = logging.getLogger(__name__)
 
@@ -14,9 +14,9 @@ class Luacheck(Tool):
 
     def check_dependencies(self):
         """
-        See if luacheck is on the system path.
+        See if luacheck image exists
         """
-        return in_path('luacheck')
+        return docker.image_exists('luacheck')
 
     def match_file(self, filename):
         base = os.path.basename(filename)
@@ -29,23 +29,15 @@ class Luacheck(Tool):
         """
         log.debug('Processing %s files with %s', files, self.name)
         command = self.create_command(files)
-        output = run_command(
-            command,
-            ignore_error=True,
-            split=True)
-        filename_converter = functools.partial(
-            self._relativize_filename,
-            files)
-        process_quickfix(self.problems, output, filename_converter)
-
-    def escape_backtick(self, problem):
-        problem.body = problem.body.replace('`', '\`')
+        output = docker.run('luacheck', command, self.base_path)
+        output = output.split("\n")
+        process_quickfix(self.problems, output, docker.strip_base)
 
     def create_command(self, files):
         command = ['luacheck']
         command += ['--formatter=plain']
         command += ['--codes']
         if self.options.get('config'):
-            command += ['--config', self.apply_base(self.options['config'])]
+            command += ['--config', docker.apply_base(self.options['config'])]
         command += files
         return command
