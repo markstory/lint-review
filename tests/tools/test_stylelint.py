3b5d9a10455a4e700805efb1feac510d8dd321a8
Mark Story <mark@mark-story.com>
2019-03-01 00:01:30 -0500

    Add coverage for fixer workflow

--- tests/tools/test_stylelint.py
+++ tests/tools/test_stylelint.py
@@ -3,7 +3,7 @@ from lintreview.review import Problems, Comment
 from lintreview.tools.stylelint import Stylelint
 from unittest import TestCase
 from nose.tools import eq_, assert_in
-from tests import root_dir, requires_image
+from tests import root_dir, read_file, read_and_restore_file, requires_image
 
 
 class TestStylelint(TestCase):
@@ -45,7 +45,7 @@ class TestStylelint(TestCase):
 
         fname = self.fixtures[1]
         error = 'Unexpected unknown at-rule "@include" (at-rule-no-unknown) [error]'
-        expected = Comment(fname, 6, 6, error)
+        expected = Comment(fname, 2, 2, error)
         eq_(expected, problems[0])
 
     @requires_image('nodejs')
@@ -70,7 +70,7 @@ class TestStylelint(TestCase):
         tool.process_files([self.fixtures[1]])
 
         problems = self.problems.all()
-        eq_(3, len(problems), 'Config file should change error count')
+        eq_(6, len(problems), 'Config file should change error count')
 
     @requires_image('nodejs')
     def test_process_files_with_invalid_config(self):
@@ -85,3 +85,24 @@ class TestStylelint(TestCase):
 
         assert_in('Your configuration file', problems[0].body)
         assert_in('ENOENT', problems[0].body)
+
+    def test_has_fixer__not_enabled(self):
+        tool = Stylelint(self.problems, {})
+        eq_(False, tool.has_fixer())
+
+    def test_has_fixer__enabled(self):
+        tool = Stylelint(self.problems, {'fixer': True}, root_dir)
+        eq_(True, tool.has_fixer())
+
+    @requires_image('nodejs')
+    def test_execute_fixer(self):
+        fixture = self.fixtures[1]
+        tool = Stylelint(self.problems, {
+            'config': 'tests/fixtures/stylelint/stylelintrc.json',
+            'fixer': True,
+        }, root_dir)
+        original = read_file(fixture)
+        tool.execute_fixer([fixture])
+
+        updated = read_and_restore_file(fixture, original)
+        assert original != updated, 'File content should change.'
