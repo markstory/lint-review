1b6d6829b6c9115794f3e9cbfac8d77e9271dc22
Farid Rener <proteusvacuum@gmail.com>
2019-03-15 11:45:31 -0400

    Add pylint command

--- lintreview/tools/py3k.py
+++ lintreview/tools/py3k.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import
-import os
+
 import logging
+
 import lintreview.docker as docker
-from lintreview.tools import Tool, process_quickfix, stringify
+from lintreview.tools import stringify
+from lintreview.tools.pylint import Pylint
 
 log = logging.getLogger(__name__)
 
 
-class Py3k(Tool):
+class Py3k(Pylint):
     """
     $ pylint --py3k is a special mode for porting to python 3 which
     disables other pylint checkers.
@@ -15,6 +17,7 @@ class Py3k(Tool):
     """
 
     name = 'py3k'
+    accepted_options = ('ignore', )
 
     def check_dependencies(self):
         """
@@ -22,45 +25,14 @@ class Py3k(Tool):
         """
         return docker.image_exists('python2')
 
-    def match_file(self, filename):
-        base = os.path.basename(filename)
-        name, ext = os.path.splitext(base)
-        return ext == '.py'
-
-    def process_files(self, files):
-        """
-        Run code checks with pylint --py3k.
-        Only a single process is made for all files
-        to save resources.
-        """
-        log.debug('Processing %s files with %s', files, self.name)
-        command = self.make_command(files)
-        output = docker.run('python2', command, self.base_path)
-        if not output:
-            log.debug('No py3k errors found.')
-            return False
-
-        output = output.split("\n")
-        output = [line for line in output if not line.startswith("*********")]
-
-        process_quickfix(self.problems, output, docker.strip_base)
-
     def make_command(self, files):
-        msg_template = '{path}:{line}:{column}:{msg_id} {msg}'
-        command = [
-            'pylint',
-            '--py3k',
-            '--reports=n',
-            '--msg-template',
-            msg_template,
-        ]
-        accepted_options = ('ignore')
+        self.check_options()
+
+        command = self._base_command
+        command.append('--py3k')
+
         if 'ignore' in self.options:
             command.extend(['-d', stringify(self.options['ignore'])])
 
-        for option in self.options:
-            if option in accepted_options:
-                continue
-            log.warning('Set non-existent py3k option: %s', option)
         command.extend(files)
         return command
---
+++ tests/fixtures/pylint/has_errors.py
@@ -0,0 +1,14 @@
+# Create unused imports
+import os, re
+
+def thing(self):
+    return  thing_two('arg1',
+     'arg2')
+
+
+def thing_two(arg1, arg2, arg3):
+    """A thinger for thinging but returning nothing
+    """
+    result=arg1*arg2
+    if result == arg1:
+        pass
